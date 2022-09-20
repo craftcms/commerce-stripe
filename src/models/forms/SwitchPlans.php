f6d70c32228e43ea5a6bb80abcd7d8b58ecba21a
preciousdaleramirez <dalefirstpage@gmail.com>
2022-09-20 19:33:04 +0800

    Fixed a bug where switching subscription plan throws a type property initialization error.

--- src/models/forms/SwitchPlans.php
+++ src/models/forms/SwitchPlans.php
@@ -22,7 +22,7 @@ class SwitchPlans extends SwitchPlansForm
      *
      * @var bool
      */
-    public bool $prorate = false;
+    public ?bool $prorate = false;
 
     /**
      * @var bool Whether the plan change should be billed immediately.
@@ -35,12 +35,12 @@ class SwitchPlans extends SwitchPlansForm
     public string|int $billingCycleAnchor;
 
     /**
-     * @var int Timestamp on which to base the proration calculation
+     * @var int|null Timestamp on which to base the proration calculation
      */
-    public int $prorationDate;
+    public ?int $prorationDate = null;
 
     /**
-     * @var int Quantity of subscription
+     * @var int|null Quantity of subscription
      */
-    public int $quantity;
+    public ?int $quantity = null;
 }
