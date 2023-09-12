04a4ffd4b69f6ec40d9a55bccf7509ad535e0391
Luke Holder <lukemh@gmail.com>
2023-09-12 10:44:42 +0800

    Pretty now

--- src/web/assets/plansettings/js/planSettings.js
+++ src/web/assets/plansettings/js/planSettings.js
@@ -1,66 +1,68 @@
 var $stripeButton = $('.stripe-refresh-plans');
 
 $stripeButton.on('click', function (ev) {
-    if ($stripeButton.hasClass('disabled')) {
-        return;
-    }
+  if ($stripeButton.hasClass('disabled')) {
+    return;
+  }
 
-    $stripeButton.addClass('disabled').siblings('.spinner').removeClass('hidden');
+  $stripeButton.addClass('disabled').siblings('.spinner').removeClass('hidden');
 
-    var gatewayId = $('.gateway-select select').val();
-    var $planSelect = $('.plan-select-' + gatewayId + ' select');
+  var gatewayId = $('.gateway-select select').val();
+  var $planSelect = $('.plan-select-' + gatewayId + ' select');
 
-    var data = {
-        gatewayId: gatewayId,
-    };
+  var data = {
+    gatewayId: gatewayId,
+  };
 
+  Craft.sendActionRequest('POST', 'commerce-stripe/plans/fetch-plans', {
+    data,
+  }).then((ev) => {
+    debugger;
 
-    Craft.sendActionRequest('POST', 'commerce-stripe/plans/fetch-plans', {data}).then((ev) => {
-        debugger;
+    const textStatus = ev.statusText;
+    const response = ev.response;
 
-        const textStatus = ev.statusText;
-        const response = ev.response;
+    $stripeButton
+      .removeClass('disabled')
+      .siblings('.spinner')
+      .addClass('hidden');
 
-        $stripeButton.removeClass('disabled').siblings('.spinner').addClass('hidden');
+    if (ev.status === 200) {
+      if (response.error) {
+        alert(response.error);
+      } else if (response.length > 0) {
+        let currentPlan = $planSelect.val(),
+          currentPlanStillExists = false;
 
-        if (ev.status === 200) {
-            if (response.error) {
-                alert(response.error);
-            } else if (response.length > 0) {
+        $planSelect.empty();
 
-                let currentPlan = $planSelect.val(),
-                    currentPlanStillExists = false;
+        for (var i = 0; i < response.length; i++) {
+          if (response[i].reference == currentPlan) {
+            currentPlanStillExists = true;
+            $planSelect.append(
+              '<option value="' +
+                response[i].reference +
+                '">' +
+                response[i].name +
+                '</option>',
+            );
+          } else {
+            $planSelect.append(
+              '<option value="' +
+                response[i].reference +
+                '">' +
+                response[i].name +
+                '</option>',
+            );
+          }
 
-                $planSelect.empty();
-
-                for (var i = 0; i < response.length; i++) {
-                    if (response[i].reference == currentPlan) {
-                        currentPlanStillExists = true;
-                        $planSelect.append(
-                            '<option value="' +
-                            response[i].reference +
-                            '">' +
-                            response[i].name +
-                            '</option>'
-                        );
-                    } else {
-                        $planSelect.append(
-                            '<option value="' +
-                            response[i].reference +
-                            '">' +
-                            response[i].name +
-                            '</option>'
-                        );
-                    }
-
-                    if (currentPlanStillExists) {
-                        $planSelect.val(currentPlan);
-                    }
-                }
-            }
+          if (currentPlanStillExists) {
+            $planSelect.val(currentPlan);
+          }
         }
-
-    });
+      }
+    }
+  });
 });
 
 $stripeButton.click();
